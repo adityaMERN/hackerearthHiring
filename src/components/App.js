b43641bb56f1a1d3c07512876b22805212bc29fe
Aditya anand <adityaanandofficial@gmail.com>
2020-11-20 19:00:20 +0530

    major bugs

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import AutoComplete from 'material-ui/AutoComplete';
 import BeerItem from './BeerItem';
@@ -10,12 +10,12 @@ class App extends Component {
             apiData: [],
             beerNames: [],
             beers: [],
-            photos: [],
+            photos:[],
             sort: ''
         };
 
         this.search = this.search.bind(this);
-
+       
     }
 
     componentDidMount() {
@@ -23,13 +23,13 @@ class App extends Component {
 
         fetch('https://s3-ap-southeast-1.amazonaws.com/he-public-data/beercraft5bac38c.json')
             .then(
-                function(response) {
+                function (response) {
                     if (response.status !== 200) {
                         return;
                     }
 
                     // Examine the text in the response
-                    response.json().then(function(data) {
+                    response.json().then(function (data) {
                         data.shift();
                         self.setState({
                             apiData: data,
@@ -38,34 +38,35 @@ class App extends Component {
                     });
                 }
             )
-            .catch(function(err) {});
-        fetch("http://s3-ap-southeast-1.amazonaws.com/he-public-data/beerimages7e0480d.json")
-            .then(
-                res => {
-                    if (!res.ok) {
-                        throw Error("Error fetching images")
-                    }
-                    return res.json()
-
-                    .then(allData => {
-                        this.setState({
-                            photos: allData,
-                        });
-
-                    });
-
-                }
-            )
-            .catch(function(err) {
-                throw Error(err.message);
+            .catch(function (err) {
             });
+        fetch("https://s3-ap-southeast-1.amazonaws.com/he-public-data/beerimages7e0480d.json")
+        .then(
+            res=>{
+                if(!res.ok){
+                    throw Error("Error fetching images")
+                }
+                return res.json()
+                
+                .then(allData=>{
+                    this.setState({
+                        photos:allData,
+                    });
+                    
+                });
+                
+            }
+        )
+        .catch(function(err){
+            throw Error(err.message);
+        });
     }
 
     search(query) {
         var beers = [];
         var beerNames = [];
 
-        this.state.apiData.forEach(function(beer) {
+        this.state.apiData.forEach(function (beer) {
             if (beer.name.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
                 beers.push(beer);
                 beerNames.push(beer.name);
@@ -78,35 +79,30 @@ class App extends Component {
         });
     }
 
-
+    
 
     render() {
-        var BeerList = this.state.beers.map(function(beer, index) {
-            return ( <
-                BeerItem key = { index }
-                data = { beer }
-                />
+        var BeerList = this.state.beers.map(function (beer, index) {
+            return (
+                <BeerItem key={index} data={beer}/>
             );
         }, this);
 
-        return ( <
-            div >
-            <
-            center >
-            <
-            div className = "margin-3" >
-            <
-            AutoComplete hintText = "Search..."
-            dataSource = { this.state.beerNames }
-            onUpdateInput = { this.search }
-            listStyle = {
-                { maxHeight: 200, overflow: 'auto' } }
-            fullWidth = { true }
-            /> <
-            /div> <
-            /center> <
-            div className = "margin-3 flex-box" > { BeerList } < /div> <
-            /div>
+        return (
+            <div>
+                <center>
+                    <div className="margin-3">
+                        <AutoComplete
+                            hintText="Search..."
+                            dataSource={this.state.beerNames}
+                            onUpdateInput={this.search}
+                            listStyle={{maxHeight: 200, overflow: 'auto'}}
+                            fullWidth={true}
+                        />
+                    </div>
+                </center>
+                <div className="margin-3 flex-box">{BeerList}</div>
+            </div>
         );
     }
 }
